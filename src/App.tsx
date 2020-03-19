2950e9dfe1602bdc75295cdcb268e143c1e018b6
jadercleber <jader.cleber@gmail.com>
2020-03-19 16:42:47 -0300

    Var para nome que cra espera

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import LiveChat from "react-livechat";
 import "./App.css";
 
 function App() {
-  console.log("process.env.LICENSE_ID", process.env.LICENSE_ID);
+  console.log("process.env.LICENSE_ID", process.env.REACT_APP_LICENSE_ID);
   return (
     <div className="App">
       <header className="App-header">
